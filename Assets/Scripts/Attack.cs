018283da976561600e5efe14d012b680cd3b1ce2
Natanael Lucena de Medeiros <nldm.eng20@uea.edu.br>
2021-06-30 11:39:48 -0400

    add characters

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+        public int damage;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+                if (other.CompareTag("Enemy"))
+                {
+                    other.gameObject.GetComponent<Life>().lifeAmount -= damage;
+                    other.gameObject.GetComponent<Life>().UpdateHealthBar();
+                    if (other.gameObject.GetComponent<Life>().lifeAmount <= 0) Destroy(other.gameObject);
+                    Destroy(gameObject);
+                }
+        }
+}
